0d2928cea56df45142634b9b6d1fd70117589910
bobbybee Scratcher <j104@rocketmail.com>
2013-01-14 19:51:08 -0500

    Added some more general generation routines

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
+var fs = require('fs');
+var projJSON = JSON.parse(fs.readFileSync(GetInflatedFile("project.json")).toString());
+console.log(GenerateAS3Code());
+
 function GetInflatedFile(f){
     return process.argv[2]+".esb2/"+f;
 }
 
+function SpaceAlt(str){
+    return str.replace(" ","sp");
+}
+
+function GenerateSpriteConstructor(sprObj){
+     var myID = SpaceAlt(sprObj.objName);
+     var retStr = "                   sprites."+myID+" = new Sprite();\n";
+     retStr += "                   addChild(sprites."+myID+");\n";
+     return retStr;
+}
+
+function GenerateMainConstructor(){
+    var retStr = "\n";
+    var i = 0;
+    while(i < projJSON.children.length){
+        retStr = retStr + GenerateSpriteConstructor(projJSON.children[i]);
+        
+        ++i;
+    }
+    retStr += "                }\n";
+    return retStr;
+}
+
 function GenerateAS3Code(){
    // return "package{\n  import flash.display.Sprite;\n\n    public class "+process.argv[2]+" extends Sprite{\n      //variables here\n\n        public function "+process.argv[2]+"():void {\n          //stuff\n       }\n     }\n}";
-    return "package{\n\
+    var retVal = "package{\n\
         import flash.display.Sprite;\n\
         \n\
         public class "+process.argv[2]+" extends Sprite{\n\
-                //variables here\n\n\
-                public function "+process.argv[2]+"():void {\n\
-                            //stuff\n\
-                }\n\
+                public var sprites:Object = new Object();\n\
+                \n\
+                public function "+process.argv[2]+"():void {";
+    retVal += GenerateMainConstructor();
+    retVal += "\
         }\n\
 }";
+    return retVal;
 
 }
 
-var fs = require('fs');
-var projJSON = JSON.parse(fs.readFileSync(GetInflatedFile("project.json")).toString());
-console.log(GenerateAS3Code());
