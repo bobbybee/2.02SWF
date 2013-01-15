7bdf5832d76daaeb026747b8412d6455a24dc19f
bobbybee Scratcher <j104@rocketmail.com>
2013-01-14 20:35:12 -0500

    Asset generation code

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var fs = require('fs');
 var projJSON = JSON.parse(fs.readFileSync(GetInflatedFile("project.json")).toString());
+var loadNum = -1;
+
+
 console.log(GenerateAS3Code());
+
 
 function GetInflatedFile(f){
     return process.argv[2]+".esb2/"+f;
@@ -10,16 +14,27 @@ function SpaceAlt(str){
     return str.replace(" ","sp");
 }
 
-function GenAssetLoadCode(path, prefix){
+function ComputeAssetName(obj){
+    var type = "baseLayer";
+    if(obj.costumeName === "undefined")
+        type = "";
+   var retVal = obj[type+"ID"];
+   var thash = obj[type+"MD5"];
+   retVal += thash.substr(thash.length - 4);
+   return retVal;
+}
+
+function GenAssetLoadCode(path, spr, prefix){
     if(typeof(prefix)==='undefined') prefix = "";
-    var retVal = prefix+"var loader:Loader = new Loader();\n"+prefix+"loader.load(new URLRequest(\""+path+"\"));\n";
+    loadNum++;
+    var retVal = prefix+"var loader"+loadNum+":Loader = new Loader();\n"+prefix+"loader"+loadNum+".contentLoaderInfo.addEventListener(Event.COMPLETE, function(e:Event){ "+spr+".beginBitmapFill(e.target.content.bitmapData);"+spr+".drawRect(0,0,e.target.content.bitmapData.width,e.taregt.content.bitmapData.height);"+spr+".endFill();});\n"+prefix+"loader"+loadNum+".load(new URLRequest(\""+path+"\"));\n";
     return retVal;
 }
 
 function GenerateSpriteConstructor(sprObj){
      var myID = SpaceAlt(sprObj.objName);
      var retStr = "                   sprites[\""+myID+"\"] = new Sprite();\n";
-     retStr += GenAssetLoadCode("./0.png", "                   ");
+     retStr += GenAssetLoadCode(ComputeAssetName(sprObj.costumes[0]),"sprites[\""+myID+"\"]","                   ");
      retStr += "                   sprites[\""+myID+"\"].x = "+(parseInt(sprObj.scratchX)+240)+";\n";
      retStr += "                   sprites[\""+myID+"\"].y = "+(parseInt(sprObj.scratchY)-180)+";\n";
      retStr += "                   addChild(sprites[\""+myID+"\"]);\n";
